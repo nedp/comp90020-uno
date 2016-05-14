38f2283935fa03cc5e90f97ef05c70ee0ba3cf18
Ned Pummeroy <nedpummeroy@gmail.com>
2016-05-14 20:48:31 +1000

    Removed an assignment I forgot to remove before

--- utility.js
+++ utility.js
@@ -4,7 +4,6 @@ var Utility = (function () {
 
   function assert(predicate, message) {
     if (predicate === false) {
-      var x = 0; // TODO
       throw 'assertion failed: ' + message;
     }
   }
